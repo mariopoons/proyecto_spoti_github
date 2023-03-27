42b50daa0f4461d471111e74b8231d5e071aadeb
mariopoons <mariopons02@gmail.com>
2023-03-27 10:38:10 +0200

    hola

---
+++ Assets/scripts/Bolacolor.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bolacolor : MonoBehaviour
+{
+    public Material _material;
+
+    private void Start()
+    {
+        _material = GetComponent<Material>();
+    }
+
+    public void RedColor()
+    {
+        _material = 
+    }
+}
